a218bcd58975dc3fb1c65b8db219531fe03d3816
Rezt1 <ivaylo20044@gmail.com>
2026-04-22 13:21:00 +0300

    modify Category entity add isDefault property make user required

--- server/src/main/java/com/example/server/entity/Category.java
+++ server/src/main/java/com/example/server/entity/Category.java
@@ -22,11 +22,14 @@ public class Category {
     private Integer id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(nullable = false, length = 80)
     private String name;
+
+    @Column(nullable = false)
+    private boolean isDefault = false;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
